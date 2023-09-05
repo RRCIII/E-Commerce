5b677618b41b50b043f6b50f5bb75d40617e49d7
Ricky Carter <randolphcarter5@gmail.com>
2023-09-05 19:10:16 -0400

    created category table to be initialized

---
+++ Models/Category.js
@@ -0,0 +1,27 @@
+const { Model, DataType } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTabkeName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
