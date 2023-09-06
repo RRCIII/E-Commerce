7fb6e10fcf8c08d9ae1c501845b01b382d29c29f
Ricky Carter <randolphcarter5@gmail.com>
2023-09-05 20:00:27 -0400

    updated Tag file

--- Models/tag.js
+++ Models/tag.js
@@ -4,4 +4,25 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-Tag.init
+Tag.init(
+    {
+        id: {
+            type: DataTypes.INTERGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        tag_name: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamp: false,
+        freezeTableName: true,
+        underscored: true, 
+        modelName: 'tag',
+    }
+);
+
+module.exports = Tag;
