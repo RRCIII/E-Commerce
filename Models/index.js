356cd169ba16f09209c758aa82480a98f701e5be
Ricky Carter <randolphcarter5@gmail.com>
2023-09-05 20:26:41 -0400

    created foreign keys to traverse reational data from different tables

--- Models/index.js
+++ Models/index.js
@@ -1,5 +1,29 @@
 // import models
 const Product = require('./Product');
-const Category = require('./Category');
+const Category = require('../Models/Category');
 const Tag = require('../Tag');
 const ProductTag = require('./ProductTag');
+
+// Product belongsTo Category
+Product.belongsTo(Category, {foreignKey: 'category_id'});
+
+// Categories hasMany Products 
+Category.hasMany(Product, {foreignKey: 'category_id'});
+
+// Products belongToMany Tags via productTag
+Product.belongsToMany(Tag, {
+    foreignKey: 'product_id',
+    through: ProductTag,
+});
+// Tag belongToMany Product via ProductTag 
+Tag.belongsToMany(Product, {
+    foreignKey: 'tag_id',
+    through: ProductTag,
+});
+
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag, 
+};
