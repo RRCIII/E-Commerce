157b93038595633ff8ad3579ecdf055a26185ae5
Ricky Carter <randolphcarter5@gmail.com>
2023-09-20 17:52:13 -0400

    updated requirements

--- routes/api/productRoutes.js
+++ routes/api/productRoutes.js
@@ -1,2 +1,2 @@
 const router = require("express").Router();
-const { Product, Category, Tag, ProductTag } = require("../../models")
+const { Product, Category, Tag, ProductTag } = require("../../models");
