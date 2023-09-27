896f7b655f4c24e11d4857e806a76d5444886e45
Ricky Carter <randolphcarter5@gmail.com>
2023-09-27 15:42:59 -0400

    updated connection.js file with correct syntax

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_Password,
+  process.env.DB_PASSWORD,
 
   {
     host: 'localhost',
